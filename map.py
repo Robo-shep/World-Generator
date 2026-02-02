ec3253385bdcdb78121155589d0b408aecd22bf8
Robo-shep <roboticsheepminecraft@gmail.com>
2026-02-02 23:36:13 +0530

    changing to gaussian

--- map.py
+++ map.py
@@ -1,12 +1,31 @@
-
-# Install PIL: pip install pillow (probably already installed)
-from PIL import Image
+import noise
 import numpy as np
-from perlin_noise import PerlinNoise
 
-noise = PerlinNoise(octaves=10, seed=1)
-xpix, ypix = 100, 100
-pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
+def fbm(x, y, octaves=5, lacunarity=2.0, gain=0.5):
+    value = 0.0
+    amplitude = 1.0
+    frequency = 1.0
 
-image = Image.fromarray(np.array(pic) * 255, 'L')
-image.save('output.png')
+    for _ in range(octaves):
+        value += amplitude * noise.pnoise2(x * frequency,y * frequency, repeatx=1024, repeaty=1024)
+        frequency *= lacunarity
+        amplitude *= gain
+
+    return value
+
+width, height = 300, 300
+scale = 0.01
+
+heightmap = np.zeros((width, height))
+
+for x in range(width):
+    for y in range(height):
+        heightmap[x][y] = fbm(x * scale, y * scale)
+
+heightmap = (heightmap - heightmap.min()) / (heightmap.max() - heightmap.min())
+
+import matplotlib.pyplot as plt
+
+plt.imshow(heightmap, cmap="terrain")
+plt.colorbar()
+plt.show()
